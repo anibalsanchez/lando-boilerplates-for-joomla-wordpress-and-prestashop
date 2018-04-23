3399d06ed8be4581b5f38bcc3885922f97205d0f
anibalsanchez <anibal.sanchez@extly.com>
2018-04-23 20:42:32 +0200

    j-dev update

--- joomla/config/j-dev.sh
+++ joomla/config/j-dev.sh
@@ -5,12 +5,17 @@ cd /app/www
 cp configuration.php configuration.php.bak
 
 cat configuration.php.bak | \
-    sed "s/\$lifetime = '[^']\+'/\$lifetime = '150'/g" | \
 	sed "s/\$mailer = '[^']\+'/\$mailer = 'smtp'/g" | \
+    sed "s/\$caching = '[^']\+'/\$caching = '0'/g" | \
+    sed "s/\$error_reporting = '[^']\+'/\$error_reporting = 'default'/g" | \
+    sed "s/\$force_ssl = '[^']\+'/\$force_ssl = '0'/g" | \
+    sed "s/\$gzip = '[^']\+'/\$gzip = '0'/g" | \
+    sed "s/\$lifetime = '[^']\+'/\$lifetime = '150'/g" | \
     sed "s/\$smtpauth = '[^']\+'/\$smtpauth = '0'/g" | \
     sed "s/\$smtphost = '[^']\+'/\$smtphost = 'mailhog'/g" | \
     sed "s/\$smtppass = '[^']\+'/\$smtppass = ''/g" | \
     sed "s/\$smtpport = '[^']\+'/\$smtpport = '1025'/g" | \
     sed "s/\$smtpsecure = '[^']\+'/\$smtpsecure = 'none'/g" | \
-    sed "s/\$smtpuser = '[^']\+'/\$smtpuser = ''/g" | \
+    sed "s/\$smtpuser = '[^']\+'/\$smtpuser = ''/g" \
     > configuration.php
+
