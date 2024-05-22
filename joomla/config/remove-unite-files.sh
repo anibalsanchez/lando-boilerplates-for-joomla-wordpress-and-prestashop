a8dd637fbb2fe5d7712a72dbc4e0206fe22f3c8c
andreagentil <andrea.gentil@extly.com>
2024-05-22 17:11:13 +0200

    First update

--- joomla/config/remove-unite-files.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-WEBROOT=$1
-JPA_FILE=${2##--jpa_filename=}".jpa"
-
-rm $WEBROOT/joomla-unite.xml
-rm $WEBROOT/unite.phar
-rm $WEBROOT/*.jpa
-echo $JPA_FILE Backup installed successfully
--- prestashop/config/post_install.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/sh
-
-rm -rf $LANDO_WEBROOT/install
-rm $LANDO_WEBROOT/prestashop.zip
-rm $LANDO_WEBROOT/prestashop_1.7.*.zip
-
-echo 'Installed! - Username: pub@prestashop.com - Password: 0123456789'
